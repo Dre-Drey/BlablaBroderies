c1e24e473a7b67a0776f3f336fb9e949190edd03
Doyen Audrey <doyenaudrey@MacBook-Pro-de-Doyen.local>
2023-03-28 15:04:28 +0200

    component collections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./components/Navbar"
 import Accueil from './components/Accueil'
+import Collections from './components/Collections'
 
 function App() {
   return (
    <>
    <Navbar/>
    <Accueil/>
+   <Collections/>
    </>
   )
 }
---
+++ src/components/Collections.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import collections from '../assets/collections.png'
+import '../styles/collections.css'
+
+export default function Collections() {
+    return (
+        <div className='blabla-collections'>
+           <h2>Collections</h2>
+        </div>
+    )
+}
