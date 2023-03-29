ba1527bd03f8458f0b3b7f594fb5bd15da8900ad
Doyen Audrey <doyenaudrey@MacBook-Pro-de-Doyen.local>
2023-03-29 15:39:34 +0200

    datas collection

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import './App.css';
 import Navbar from "./components/Navbar"
 import Accueil from './components/Accueil'
 import Collections from './components/Collections'
+import {data} from './data/data'
 
 function App() {
   return (
    <>
    <Navbar/>
    <Accueil/>
-   <Collections/>
+   <Collections
+   punchline="blabla"
+   collection = "monde d'apres"
+   couleur = "rose"
+   broderie = "violet"
+   prix = "28"
+   />
    </>
   )
 }
--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import collections from '../assets/collections.png'
 import '../styles/collections.css'
 
-export default function Collections() {
+export default function Collections({punchline, collection, couleur, broderie, prix}) {
     return (
         <div className='blabla-collections'>
            <h2>Collections</h2>
+           <div className='blabla-item'>
+            <h3>"{punchline}"</h3>
+            <p>Collection : {collection}</p>
+            <p>Couleur du t-shirt :  {couleur}</p>
+            <p>Couleur de la broderie : {broderie}</p>
+            <p>Prix : {prix} euros</p>
+           </div>
         </div>
     )
 }
